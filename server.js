9ec095f7d93a556276b5a6d375dc4bf3910bd962
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-04-16 18:04:01 +0530

    Bug fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get(express.static('client/build'));
+app.use(express.static('client/build'));
 
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
