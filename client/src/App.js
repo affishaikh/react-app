406b9fbe9cd1b0da7870399adc6d1b7599949a55
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-04-16 18:31:22 +0530

    Create a counter

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  componentDidMount() {
+    fetch('/count')
+      .then(res => {
+        res.json();
+      })
+      .then(json => {
+        this.setState(() => {
+          const counter = json.counter;
+          return { counter };
+        });
+      });
+  }
+
   render() {
-    return <h1>My name is Uzumaki Naruto</h1>;
+    return (
+      <h1 onClick={this.naruto.bind(this)}>
+        My name is Uzumaki Naruto {this.state.counter}
+      </h1>
+    );
   }
 }
 
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
+let count = 2;
 
 app.use(express.static('client/build'));
+
+app.get('/count', (req, res) => {
+  res.send(JSON.stringify({ counter: count }));
+});
 
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
