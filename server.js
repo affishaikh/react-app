fdcfcb2c96cfddc3d2afe2231dec57ac1107575c
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-04-16 17:52:51 +0530

    Add express.static

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('<h1>My name is Uzumaki Naruto</h1>');
-});
+app.get('/', express.static('client/build'));
 
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
