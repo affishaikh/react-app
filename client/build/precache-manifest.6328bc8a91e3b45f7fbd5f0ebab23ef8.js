2bc4532b4d4229e50dd2377c97ec5b118d3b86c6
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-04-16 17:46:12 +0530

    Create a react app

---
+++ client/build/precache-manifest.6328bc8a91e3b45f7fbd5f0ebab23ef8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "21792937edcb7afdd7f8",
+    "url": "/static/js/main.94102af8.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "21792937edcb7afdd7f8",
+    "url": "/static/css/main.febf993d.chunk.css"
+  },
+  {
+    "revision": "ca8ce4201c11a57dc0087f3a473d0c14",
+    "url": "/index.html"
+  }
+];
